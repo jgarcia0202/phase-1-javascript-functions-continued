76fd71377e6a15d4c66e0b51fb35ade15894ce59
Jose <jmgarcia0202@gmail.com>
2022-05-11 19:05:14 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(event = "roller-skate") {
+  return `This Saturday, I want to ${event}!`
+}
+function mondayWork(event = 'go to the office') {
+  return `This Monday, I will ${event}.`
+}
+function wrapAdjective(emph = "*") {
+  const result = function(sent){
+    return `You are ${emph}${sent}${emph}!`
+  }
+  return result
+
+}
